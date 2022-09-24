8460eebc73ab50dd1bc36b36ca42a73617416174
I506dk <33561466+I506dk@users.noreply.github.com>
2022-09-24 00:23:37 -0500

    Update adpop.py

--- adpop.py
+++ adpop.py
@@ -1,10 +1,6 @@
-import names
 import string
 import random
 import subprocess
-import area_code_nanp
-import random_address
-
 
 # Function to install packages via pip (aka Pip Cheat)
 def install_library(package):
@@ -18,6 +14,11 @@ libraries = ["names", "py-area-code-nanp", "random_address"]
 # Install libraries
 for library in libraries:
     install_library(library)
+    
+import names
+import area_code_nanp
+import random_address
+
 
 # Number of users to create (default is 10k)
 global user_count
@@ -117,15 +118,18 @@ def create_ou(domain, departments):
         else: 
             dc_domain += "DC=" + str(domain_pieces[i])
         i += 1
-        
+    
+    # Create list of departments
     ou_list = list(departments.keys())
+        
+    # Add in checks for OU existance    
         
     # Create OUs with powershell
     for item in ou_list:
         #print(item) 
         ou_command = "New-ADOrganizationalUnit -Name '{}' -Path '{}'".format(str(item), dc_domain)
         print(ou_command)
-        #subprocess.check_output(["powershell.exe", ou_command])
+        subprocess.check_output(["powershell.exe", ou_command])
     
     return
 
@@ -299,23 +303,19 @@ def create_users(domain, company_name, departments, number_of_users):
     # Create all Active Directory users
     i = 0
     while i < int(number_of_users):
-        # Organization unit (default is People)
-        organization_unit = "People"
+        # Organization unit (default is Users)
+        organization_unit = "Users"
 
         # Generate active directory attributes for a given user
         ad_user = generate_ad_user(domain, organization_unit, company_name, departments)
         #print(ad_user["Password"])
 
-        # User powershell to convert plain test password to secure object
-        password_command = "$securePassword = ConvertTo-SecureString -AsPlainText '{}' -Force".format(ad_user["Password"])
-        #print(password_command)
-        #subprocess.check_output(["powershell.exe", password_command])
-
         # Powershell command to actually create the user
-        user_command = "New-ADUser -SamAccountName '{}' -Name '{}' -Path '{}' -AccountPassword $securePassword -Enabled $true -GivenName '{}' -Surname '{}' -DisplayName '{}' -EmailAddress '{}' -StreetAddress '{}' -City '{}' -PostalCode '{}' -State '{}' -Country '{}' -UserPrincipalName '{}' -Company '{}' -Department '{}' -Title '{}' -OfficePhone '{}' -PasswordNeverExpires $true -ChangePasswordAtLogon $false".format(
+        user_command = "New-ADUser -SamAccountName '{}' -Name '{}' -Path '{}' -AccountPassword (ConvertTo-SecureString -AsPlainText '{}' -Force) -Enabled $true -GivenName '{}' -Surname '{}' -DisplayName '{}' -EmailAddress '{}' -StreetAddress '{}' -City '{}' -PostalCode '{}' -State '{}' -Country '{}' -UserPrincipalName '{}' -Company '{}' -Department '{}' -Title '{}' -OfficePhone '{}' -PasswordNeverExpires $true -ChangePasswordAtLogon $false".format(
             ad_user["SamAccountName"],
             ad_user["Name"],
             ad_user["Path"],
+            ad_user["Password"],
             ad_user["GivenName"],
             ad_user["Surname"],
             ad_user["DisplayName"],
@@ -331,8 +331,9 @@ def create_users(domain, company_name, departments, number_of_users):
             ad_user["Title"],
             ad_user["OfficePhone"]
         )
-        print(user_command)
-        #subprocess.check_output(["powershell.exe", user_command])
+
+        # Create user
+        subprocess.check_output(["powershell.exe", user_command])
         
         i += 1
 
@@ -350,14 +351,12 @@ def create_ou_users(domain, company_name, departments, number_of_users):
         # Generate active directory attributes for a given user
         ad_user = generate_ad_user(domain, organization_unit, company_name, departments)
         
-        # User powershell to convert plain test password to secure object
-        password_command = "$securePassword = ConvertTo-SecureString -AsPlainText '{}' -Force".format(ad_user["Password"])
-    
         # Powershell command to actually create the user
-        user_command = "New-ADUser -SamAccountName '{}' -Name '{}' -Path '{}' -AccountPassword $securePassword -Enabled $true -GivenName '{}' -Surname '{}' -DisplayName '{}' -EmailAddress '{}' -StreetAddress '{}' -City '{}' -PostalCode '{}' -State '{}' -Country '{}' -UserPrincipalName '{}' -Company '{}' -Department '{}' -Title '{}' -OfficePhone '{}' -PasswordNeverExpires $true -ChangePasswordAtLogon $false".format(
+        user_command = "New-ADUser -SamAccountName '{}' -Name '{}' -Path '{}' -AccountPassword (ConvertTo-SecureString -AsPlainText '{}' -Force) -Enabled $true -GivenName '{}' -Surname '{}' -DisplayName '{}' -EmailAddress '{}' -StreetAddress '{}' -City '{}' -PostalCode '{}' -State '{}' -Country '{}' -UserPrincipalName '{}' -Company '{}' -Department '{}' -Title '{}' -OfficePhone '{}' -PasswordNeverExpires $true -ChangePasswordAtLogon $false".format(
             ad_user["SamAccountName"],
             ad_user["Name"],
             ad_user["Path"],
+            ad_user["Password"],
             ad_user["GivenName"],
             ad_user["Surname"],
             ad_user["DisplayName"],
@@ -373,8 +372,9 @@ def create_ou_users(domain, company_name, departments, number_of_users):
             ad_user["Title"],
             ad_user["OfficePhone"]
         )
-        print(user_command)
-        #subprocess.check_output(["powershell.exe", user_command])
+
+        # Create user
+        subprocess.check_output(["powershell.exe", user_command])
         
         i += 1
     
